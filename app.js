0519c3e9c48aa07f4f3eea03acb692a97fa6fcf0
nirzak <nirjas01@student.sust.edu>
2021-05-22 22:42:34 +0600

    Variable types are changed

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var loginRouter = require('./routes/login');
-var regRouter = require('./routes/reg');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const loginRouter = require('./routes/login');
+const regRouter = require('./routes/reg');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use(loginRouter);
@@ -43,6 +44,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-var PORT = process.env.PORT || 5000;
+// Starting server with port
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running on  ${PORT}`));
--- routes/reg.js
+++ routes/reg.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET Student Sign Up Page */
 router.get('/student-signup', function(req, res, next) {
