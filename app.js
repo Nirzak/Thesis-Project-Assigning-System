6a8926cb7b216aca1826f5a1091b686e865051a6
nirzak <nirjas01@student.sust.edu>
2021-05-22 22:35:14 +0600

    Initiating Authentication

--- app.js
+++ app.js
@@ -43,4 +43,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+var PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running on  ${PORT}`));
